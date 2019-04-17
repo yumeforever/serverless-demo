9de3b0d59c95ad35c2af782c9756992d19e96d6f
yumeforever <morganadalmeida@de.ibm.com>
2019-04-17 11:59:18 +0200

    test 2

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ module.exports = async function (context, req) {
     else {
         context.res = {
             status: 400,
-            body: "Please lalalalla a name on the query string or in the request body"
+            body: "Please sjhashdkjhask a name on the query string or in the request body"
         };
     }
 };
